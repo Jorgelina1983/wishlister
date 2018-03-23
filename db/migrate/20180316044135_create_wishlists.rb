1e3425f291735fc04d7c8b6196b3e96ea0a3d43b
Jorgelina1983 <jcorral@intouchhealth.com>
2018-03-22 21:54:24 -0300

    Fix migration.

--- db/migrate/20180316044135_create_wishlists.rb
+++ db/migrate/20180316044135_create_wishlists.rb
@@ -4,7 +4,7 @@ class CreateWishlists < ActiveRecord::Migration[5.0]
       t.string :venue_id
       t.string :venue_photo
       t.string :venue_name
-      # t.references :user, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
